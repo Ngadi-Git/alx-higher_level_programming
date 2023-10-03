8cb01ac1dd94d686e1a6902f8e9eefc57ef303a8
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-03 00:35:26 +0000

    Updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,24 @@
 #include "lists.h"
 
 /**
- *check_cycle - checks if a linked list contains a cycle
- *@list: linked list to check
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- *Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-
-	listint_t *eleph, *cheetah;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-		
-	listint_t *eleph = list;
-	listint_t *cheetah = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
 
-
-	for (; eleph && cheetah && cheetah->next; eleph = eleph->next, cheetah = cheetah->next->next)
+	while (slow && fast && fast->next)
 	{
-		if (eleph == cheetah)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
 
