d0fd7a9f50a132ebecc5847bd75c6ce8ee000be7
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-04 20:07:56 +0000

    Updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@
 index = 0
 for char in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(char - index)), end="")
-    index = 32 if index == 0 `else 0
+    index = 32 if index == 0 else 0
