c079a37259c9de6d2927a5bae5380265ab504077
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-05 21:35:30 +0000

    Task 102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    from magic_calculatiion_102 import add, sub
+
+    if a < b:
+        sum = add(a, b)
+        for index in range(4, 6):
+            sum = ad    d(sum, index)
+        return (sum)
+
+    else:
+        return(sub(a, b))
