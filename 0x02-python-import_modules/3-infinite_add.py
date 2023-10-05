28ae0b7c8848ecd2cf4f296eb841d55dc1d5deff
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-05 20:58:16 +0000

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 
     sum = 0
     for index in range(len(sys.argv) - 1):
-        sum = sum + int(sys.argv[index + 1])
+        sum += int(sys.argv[index + 1])
     print("{}".format(sum))
