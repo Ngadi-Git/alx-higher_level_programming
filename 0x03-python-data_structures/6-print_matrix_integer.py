e3f87294416adac3b81c967970ab463852e406c0
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-08 21:43:43 +0000

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for index in range(len(matrix)):
+        for jdx in range(len(matrix[index])):
+                print("{:d}".format(matrix[index][jdx]), end="")
+                if jdx != (len(matrix[index]) - 1):
+                    print(" ", end="")
+
+        print("")
