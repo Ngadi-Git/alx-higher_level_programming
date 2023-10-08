4b829a133aba156e44a6f7326fafde083be76890
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-08 21:50:46 +0000

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for r in matrix:
-        for c in row:
-            if c == r[-1]:
-                print('{:d}'.format(c), end='')
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
             else:
-                print('{:d}'.format(c), end=' ')
+                print('{:d}'.format(column), end=' ')
         print()
