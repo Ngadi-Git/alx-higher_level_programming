712d8fb06ef45ca93d4074e30fe5f2d6e6667d4e
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-11 22:52:21 +0000

    Task100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        digit = 0
-        bottom = 0
-        for top in my_list:
-            digit += (top[0] * top[1])
-             += (top[1])
-        return (digit/bottom)
+        num = 0
+        denom = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            denom += (tup[1])
+        return (num/denom)
     return 0
