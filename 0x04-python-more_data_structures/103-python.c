97b03be1c32e9dfadc82b9b0ecb46bfe8ae09a91
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-13 20:31:28 +0000

    Updated T11

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -34,12 +34,12 @@ void print_python_list(PyObject *p)
 }
 
 /**
- * print_python_bytes - Prints basic info about Python byte objects.
+ * print_python_bytes - Prints basic info about the byte objects.
  * @p: A PyObject byte object.
  */
 void print_python_bytes(PyObject *p)
 {
-	unsigned char i, size;
+	unsigned char index, bfr;
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
@@ -53,15 +53,15 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", bytes->ob_sval);
 
 	if (((PyVarObject *)p)->ob_size > 10)
-		size = 10;
+		bfr = 10;
 	else
-		size = ((PyVarObject *)p)->ob_size + 1;
+		bfr = ((PyVarObject *)p)->ob_size + 1;
 
-	printf("  first %d bytes: ", size);
-	for (i = 0; i < size; i++)
+	printf("  first %d bytes: ", bfr);
+	for (index = 0; index < bfr; index++)
 	{
-		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (size - 1))
+		printf("%02hhx", bytes->ob_sval[index]);
+		if (index == (bfr - 1))
 			printf("\n");
 		else
 			printf(" ");
