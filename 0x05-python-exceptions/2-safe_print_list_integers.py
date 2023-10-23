3fb025319873c3ebd134c29d6cda1b20f414c83a
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 23:13:13 +0000

    All tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 2-safe_print_list_integers.py
 
-ef safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     tot = 0
     for index in range(0, x):
         try:
