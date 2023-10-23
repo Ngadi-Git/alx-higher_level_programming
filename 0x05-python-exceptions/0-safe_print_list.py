065f4809b80a0357feb26a582cf0531212e39626
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 22:40:23 +0000

    Readme

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-def safe_print_liist(my_list=[], x=0):
-    total = 0
-    for index in range(x):
+def safe_print_list(my_list=[], x=0):
+    count_total = 0
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
-            total += 1
+            print(my_list[index], end='')
+            count_total += 1
         except IndexError:
             break
-    print("")
-    return (total)
+    print('')
+    return (count_total)
