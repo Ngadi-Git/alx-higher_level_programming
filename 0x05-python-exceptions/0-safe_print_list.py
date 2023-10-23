d44eb55d4cf40270cc066c7edbe6f09808305e5e
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 22:34:42 +0000

    Readme

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+
+def safe_print_liist(my_list=[], x=0):
     total = 0
     for index in range(0, x):
         try:
             print(my_list[index], end='')
             total += 1
-        except:
+        except IndexError:
             break
     print('')
-    return total
+    return (total)
