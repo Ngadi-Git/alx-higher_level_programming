53accdd873f833ab5629e93e29914404ea32d98c
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 23:07:02 +0000

    All tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     tot = 0
-    
     for index in range(x):
-        try:
-            print("{}".format(my_list[index]), end="")
-            tot += 1
-        except IndexError:
-            break
+    try:
+        print("{}".format(my_list[index]), end="")
+        tot += 1
+    except IndexError:
+        break
     print("")
     return (tot)
