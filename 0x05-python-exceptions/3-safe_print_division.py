c63cc58091ae8bedfd2ae5cd4cbe6b4a3e949ee2
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 23:14:05 +0000

    All tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 3-safe_print_division.py
 
-def safe_print_division(a, b):
+ef safe_print_division(a, b):
     try:
         quotnt = a / b
     except (TypeError, ZeroDivisionError):
