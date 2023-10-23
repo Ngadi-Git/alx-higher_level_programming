d9d5f3c00feec20cce5b1b6d8b4fdfbb16153a68
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 22:50:54 +0000

    Readme

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError):
         return (True)
-    else:
+    except (TypeError, ValueError):
         return (False)
