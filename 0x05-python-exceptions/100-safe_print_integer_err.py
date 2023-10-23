c5669f6aab72dff4ecaa70d3ac55011c69cfcf9e
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-23 23:23:10 +0000

    All tasks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-try:
+    try:
         print("{:d}".format(value))
         return (True)
-    except (TypeError, ValueError) as e:
+    except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
