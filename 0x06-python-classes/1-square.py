90670a4fa15e2f6ef8e89dcd5614ec4ab60b28be
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-24 22:53:35 +0000

    All tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 # 1-square.py
+"""Definnge a class, Square."""
 
 
 class Square:
+    """Represents a square class."""
 
     def __init__(self, size):
+        """Constructor that initializes a new Square.
 
+        Args:
+            size (int): The size of the new square object.
+        """
         self.__size = size
