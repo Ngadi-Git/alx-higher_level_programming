52f986c0fe4f3608ab1f9b3a3c3e398147ba19dd
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-24 23:40:35 +0000

    All tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,70 +1,59 @@
 #!/usr/bin/python3
-"""Module containing the Square class"""
+"""Defining a class Square."""
 
 
 class Square:
-    """The Square class"""
+    """Represents a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initializing an instance of Square
+        """Initializes a new square instance.
 
         Args:
-            size (int): The size of the Square instance. Default value is 0.
-            position (:obj:'tuple' of int): x, y coordinate offset when
-                printing the square.
+            size (int): The size of the new square object.
+            position (int, int): The position of the new square of the object.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """int: Value of 'size'"""
-        return self.__size
+        """Get the current size of the square object."""
+        return (self.__size)
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
-        """:obj:'tuple' of int: x, y coordinate offset when printing the square.
-        """
-        return self.__position
+        """Get the current position of the square object."""
+        return (self.__position)
 
     @position.setter
-    def position(self, position):
-        if (not isinstance(position, tuple) or len(position) is not 2 or
-            not isinstance(position[0], int) or
-                not isinstance(position[1], int) or position[0] < 0 or
-                position[1] < 0):
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-
-        self.__position = position
+        self.__position = value
 
     def area(self):
-        """Returns the current square area of the instance
-
-        Returns:
-            int: Value of 'size'
-        """
-        return self.__size ** 2
+        """Returns the current area of the square object."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Prints a square with hashtags using the 'size'. Uses 'position' to
-        offset where the printing should begin. The x, y coordinate in
-        'position' is the location of the top left corner of the square."""
-        if self.__size is not 0:
-            for y in range(self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print("")
-        else:
+        """Prints the square with the # symbol/charcter."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
