b77e0cea0f1135e6a3341407363ef76fe0977bcf
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-24 00:13:46 +0000

    All tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,12 +3,12 @@
 
 def magic_calculation(a, b):
     result = 0
-    for index in range(1, 3):
+    for i in range(1, 3):
         try:
-            if index > a:
+            if i > a:
                 raise Exception('Too far')
             else:
-                result += a ** b / index
+                result += a ** b / i
         except:
             result = b + a
             break
