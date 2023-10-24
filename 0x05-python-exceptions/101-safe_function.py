6fbc9f9ecb21d47b0e2910723d0103efb7668eff
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-24 00:19:32 +0000

    All tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,11 @@ import sys
 
 
 def safe_function(fct, *args):
+    result = None
     try:
         result = fct(*args)
-        return result
-    except:
-        sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
+    except Exception as err:
+        sys.stderr.write("Exception: " + err.__str__() + "\n")
         return None
+    else:
+        return (result)
