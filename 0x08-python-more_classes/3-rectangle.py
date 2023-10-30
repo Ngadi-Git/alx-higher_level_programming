c6f008e39029bbacecfaffe71dd02e626fcc17b7
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-30 18:10:51 +0000

    All tasks

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -61,9 +61,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
+        rectObj = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rectObj.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+                rectObj.append("\n")
+        return ("".join(rectObj))
