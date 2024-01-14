4b198f0285703a0da0789ea60b31ff60151602d0
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-14 19:46:31 +0000

    All Tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-script to list all states from hbtn_0e_0_usa matching argument
-"""
-
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sqlalchemy
-    from sys import argv
-
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    cur = db.cursor()
-
-    cur.execute(
-        "SELECT * FROM states WHERE BINARY states.name = '{}' ORDER BY \
-        states.id".format(argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
