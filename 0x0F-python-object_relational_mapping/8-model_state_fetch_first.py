d9addb93384f298a2ba378dc9f52dc4213de8aa6
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-18 20:42:56 +0000

    All tasks

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """
-This script prints the first State object
-from the database `hbtn_0e_6_usa`.
+script to list first State objects
 """
+
 
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-    instance = session.query(State).order_by(State.id).first()
-
-    if instance is None:
-        print('Nothing')
+    state = session.query(State).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print('{0}: {1}'.format(instance.id, instance.name))
+        print("Nothing")
+    session.close()
