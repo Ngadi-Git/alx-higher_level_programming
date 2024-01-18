459aad59a85f691f53d121eefceb55acac1f3f9e
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-18 20:11:42 +0000

    All tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
-import MySQLdb
+"""
+script to list all states from hbtn_0e_0_usa matching argument
+"""
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    import MySQLdb
+    import sqlalchemy
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    if ';' not in argv[4]:
+        cur.execute(
+            "SELECT * FROM states WHERE states.name = '{}' ORDER BY \
+            states.id".format(argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    db.close()
