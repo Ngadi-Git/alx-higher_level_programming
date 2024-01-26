db3907cbd9997f6d7bf278f2eaf8d918bba61bbd
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-26 23:52:41 +0100

    All tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Fetch URL using requests package"""
+"""Fetches https://intranet.hbtn.io/status."""
+import requests
+
 
 if __name__ == "__main__":
-    from requests import get
-
-    try:
-        r = get('https://intranet.hbtn.io/status')
-        print('Body response:')
-        print('\t- type: {}'.format(type(r.text)))
-        print('\t- content: {}'.format(r.text))
-    except Exception as e:
-        print(e)
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
