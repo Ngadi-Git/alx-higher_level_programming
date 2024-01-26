31829684bf95aabc1958d3af0c2a1bdb35f1a11d
root <Ngadi-Git>
2024-01-25 18:04:03 -0800

    All tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of integers.
-
-    Example:
-        [1, 2, 4, 6, 3] -> 6
-        [4, 2, 1, 2, 3, 1] -> 3 # but 4 is also a peak
-
-    Returns: integer that is the peak.
     """
-    l = len(list_of_integers)
-    n = l // 2
-    v = 0
-    while v < l:
-        v += 1
-        if n == 0:
-            gtl = True
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
-            gtl = False
-        if n == l - 1:
-            gtr = True
-        else:
-            gtr = False
-        if n > 0:
-            if list_of_integers[n - 1] < list_of_integers[n]:
-                gtl = True
-        if n < l - 1:
-            if list_of_integers[n] > list_of_integers[n + 1]:
-                gtr = True
-        if gtl and gtr:
-            return list_of_integers[n]
-        if not gtr:
-            n += (l - n) // 2
-        elif not gtl:
-            n -= n // 2
+            return list_of_integers[mid]
