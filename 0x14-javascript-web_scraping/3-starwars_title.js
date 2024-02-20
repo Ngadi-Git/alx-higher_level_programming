bffd0b2d6e32a635888a9fad866ef79ab7e25cfb
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-02-20 23:50:48 +0100

    All tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+request.get('http://swapi.co/api/films/' + process.argv[2] + '/', (err, response, body) => {
+  if (err) console.log(err);
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
 });
