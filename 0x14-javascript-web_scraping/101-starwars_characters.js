7b3c9803680c954cb2e4a1536cb65d21e7eb1d6c
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-02-20 23:59:28 +0100

    All tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('http://swapi.co/api/films/' + process.argv[2], (err, resp, body) => {
-  if (err) console.log(err);
-  else if (resp.statusCode === 200) {
-    let film = JSON.parse(body);
-    let promises = [];
-    for (let ch of film.characters) {
-      promises.push(new Promise((resolve, reject) => {
-        request.get(ch, (err, resp, body) => {
-          if (err) {
-            reject(err);
-          } else if (resp.statusCode === 200) {
-            resolve(JSON.parse(body).name);
-          } else {
-            reject(Error('Unknown'));
-          }
-        });
-      }));
-    }
-    Promise.all(promises).then((names) => names.forEach((name) => console.log(name)));
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
